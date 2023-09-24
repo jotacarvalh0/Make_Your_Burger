0b237467ca2fa7106656d7f0b542b41cbf56f956
jotacarvalh0 <josecarvalho11235@gmail.com>
2023-09-24 13:25:44 -0300

    STYLE: Estilização do componente [BurgerForm]

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -1,7 +1,7 @@
 <template>
     <p>Componente de Mensagem</p>
     <div>
-        <form id="burguer-form">
+        <form id="burger-form">
             <div class="input-container">
                 <label for="name">Nome do cliente: </label>
                 <input type="text" id="name" name="name" :="name" placeholder="Digite o seu nome">
@@ -20,9 +20,17 @@
                     <option value="maminha">Maminha</option>
                 </select>
             </div>
-            <div class="input-container">
-                <label for="optional">Selecione os opcionais: </label>
-                <div class="chackbox-container">
+            <div id="optional-container" class="input-container">
+                <label id="optional-title" for="optional">Selecione os opcionais: </label>
+                <div class="checkbox-container">
+                    <input type="checkbox" name="optional" :="optional" value="bacon">
+                    <span>Bacon</span>
+                </div>
+                <div class="checkbox-container">
+                    <input type="checkbox" name="optional" :="optional" value="salada">
+                    <span>Salada</span>
+                </div>
+                <div class="checkbox-container">
                     <input type="checkbox" name="optional" :="optional" value="salame">
                     <span>Salame</span>
                 </div>
@@ -40,6 +48,69 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+    #burger-form {
+        max-width: 400px;
+        margin: 0 auto;
+    }
 
+    .input-container {
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 20px;
+    }
+
+    label {
+        font-weight: bold;
+        margin-bottom: 15px;
+        color: #222;
+        padding: 5px 10px;
+        border-left: 4px solid #FCBA03;
+    }
+
+    input, select {
+        padding: 5px 10px;
+        width: 300px;
+    }
+
+    #optional-container {
+        flex-direction: row;
+        flex-wrap: wrap;
+    }
+
+    #optional-title {
+        width: 100%;
+    }
+
+    .checkbox-container {
+        display: flex;
+        align-items: flex-start;
+        width: 50%;
+        margin-bottom: 20px;
+    }
+
+    .checkbox-container span,
+    .checkbox-container input {
+        width: auto;
+    }
+    .checkbox-container span{
+        margin-left: 6px;
+        font-weight: bold;
+    }
+
+    .submit-btn {
+        background-color: #222;
+        color: #FCBA03;
+        font-weight: bold;
+        border: 2px solid #222;
+        padding: 10px;
+        font-size: 16px;
+        margin: 0 auto;
+        cursor: pointer;
+        transition: .5s;
+    }
+    .submit-btn:hover {
+        background-color: transparent;
+        color: #222;
+    }
 </style>
