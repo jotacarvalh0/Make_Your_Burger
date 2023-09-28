35ee362ac36d5d3049e9ccd4794724f9cd3a124f
jotacarvalh0 <josecarvalho11235@gmail.com>
2023-09-28 13:28:59 -0300

    FEAT: Resgatando pedidos do campo e inserindo nos pedidos de forma dinâmica.

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -11,33 +11,16 @@
             </div>
         </div>
         <div id="burger-table-rows">
-            <div class="burger-table-row">
-                <div class="order-number">1</div>
-                <div>João</div>
-                <div>Pão de trigo</div>
-                <div>Maminha</div>
+            <div class="burger-table-row" v-for="burger in burgers" :key="burger.id">
+                <div class="order-number">{{ burger.id }}</div>
+                <div>{{ burger.name }}</div>
+                <div>{{ burger.bread }}</div>
+                <div>{{ burger.meat }}</div>
                 <div>
                     <ul>
-                        <li>Salame</li>
-                        <li>Bacon</li>
-                    </ul>
-                </div>
-                <div>
-                    <select name="status" class="status">
-                        <option value="">Selecione</option>
-                    </select>
-                    <button class="delete-btn">Cancelar</button>
-                </div>
-            </div>
-            <div class="burger-table-row">
-                <div class="order-number">1</div>
-                <div>João</div>
-                <div>Pão de trigo</div>
-                <div>Maminha</div>
-                <div>
-                    <ul>
-                        <li>Salame</li>
-                        <li>Bacon</li>
+                        <li v-for="(optional, index) in burger.optionals" :key="index">
+                            {{ optional }}
+                        </li>
                     </ul>
                 </div>
                 <div>
@@ -53,7 +36,29 @@
 
 <script>
 export default {
-    name: "Dashboard"
+    name: "Dashboard",
+    data() {
+        return{
+            burgers: null,
+            burger_id: null,
+            status: []
+        }
+    },
+    methods: {
+        async getPedidos() {
+
+            const req = await fetch('http://localhost:3000/burgers')
+
+            const data = await req.json()
+
+            this.burgers = data;
+
+            //resgatar os status.
+        }
+    },
+    mounted() {
+    this.getPedidos()
+    }
 }
 </script>
 
